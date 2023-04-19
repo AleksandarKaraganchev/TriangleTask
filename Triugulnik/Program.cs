475b7244d3fdc5b22b1dbf63ba9e3e23072864ec
AleksandarKaraganchev <aleksandar.karaganchev546@edu1.pgzaimov.com>
2023-04-19 20:38:51 +0300

    First commit

---
+++ Triugulnik/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Triugulnik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Въведи страна а на триъгълника: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Въведи височина h на триъгълника: ");
+            double h = double.Parse(Console.ReadLine());
+            Triugulnik triangle = new Triugulnik(a, h);
+            Console.WriteLine($"Триъгълник e със страна --> {triangle.PrintA()}, и височина, която е към нея  --> {triangle.PrintH()}");
+            triangle.Print();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Triugulnik/Triugulnik.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Triugulnik
+{
+    public class Triugulnik
+    {
+        private double a;
+        private double h;
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public double H
+        {
+            get { return h; }
+            set { h = value; }
+        }
+        public Triugulnik(double a , double h)
+        {
+            this.a = a;
+            this.h = h;
+        }
+        public double FaceOfTriangle()
+        {
+            double result;
+            result = a * h / 2;
+            return result;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Страна = {a} и височина към нея = {h} , са с лице {FaceOfTriangle()}");
+        }
+        public double PrintA()
+        {
+            return a;
+        }
+        public double PrintH()
+        {
+            return h;
+        }
+    }
+}
